fbe67567ddb85e74e543a1a117429399f21ce9b7
Christopher Dell <chris@tigrish.com>
2012-01-14 16:23:20 +0100

    Rename "locale" dir to "locales"

--- lib/devise-i18n.rb
+++ lib/devise-i18n.rb
@@ -3,7 +3,7 @@ require 'rails'
 module DeviseI18n
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'rails-i18n' do |app|
-      I18n.load_path << Dir[File.join(File.expand_path(File.dirname(__FILE__) + '/../locale'), '*.yml')]
+      I18n.load_path << Dir[File.join(File.expand_path(File.dirname(__FILE__) + '/../locales'), '*.yml')]
       I18n.load_path.flatten!
     end
   end
--- spec/devise-i18n_spec.rb
+++ spec/devise-i18n_spec.rb
@@ -1,8 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-Dir.glob('locale/*.yml').each do |locale_file|
+Dir.glob('locales/*.yml').each do |locale_file|
   describe 'a locale file' do
     it_behaves_like 'a valid locale file', locale_file
-    it { locale_file.should be_a_subset_of 'locale/en-US.yml' }
+    it { locale_file.should be_a_subset_of 'locales/en-US.yml' }
   end
 end
