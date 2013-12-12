5bea860c78efed82d7f2935aed569b96b7b8eb0e
Joel AZEMAR <joel.azemar@gmail.com>
2013-12-12 09:20:26 +0100

    don't modify available locales app

--- lib/devise-i18n.rb
+++ lib/devise-i18n.rb
@@ -3,7 +3,14 @@ require 'rails'
 module DeviseI18n
   class Railtie < ::Rails::Railtie #:nodoc:
     initializer 'rails-i18n' do |app|
-      I18n.load_path << Dir[File.join(File.expand_path(File.dirname(__FILE__) + '/../locales'), '*.yml')]
+      I18n.available_locales.each do |available_locale|
+        locale_file_path = File.join(File.expand_path(
+          File.dirname(__FILE__) + '/../locales'), "available_locale.yml"
+        )
+        if File.exist?(locale_file_path)
+          I18n.load_path << locale_file_path
+        end
+      end
       I18n.load_path.flatten!
     end
   end
