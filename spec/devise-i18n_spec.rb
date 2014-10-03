7284fce8fdd075271b87a99236eb478e028566bb
Christopher Dell <chris@tigrish.com>
2014-10-03 02:23:03 +0200

    Fix path to yml files in specs

--- spec/devise-i18n_spec.rb
+++ spec/devise-i18n_spec.rb
@@ -1,8 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-Dir.glob('locales/*.yml').each do |locale_file|
+Dir.glob('config/locales/*.yml').each do |locale_file|
   describe "a devise-i18n #{locale_file} locale file" do
     it_behaves_like 'a valid locale file', locale_file
-    it { locale_file.should be_a_subset_of 'locales/en.yml' }
+    it { locale_file.should be_a_subset_of 'config/locales/en.yml' }
   end
 end
