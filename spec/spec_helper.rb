d56d85a1c99b999da2a0e124db167f9f89909837
David Rodríguez <deivid.rodriguez@gmail.com>
2015-05-06 21:31:54 -0300

    Remove empty RSpec configure block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,3 @@ require 'i18n-spec'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-RSpec.configure do |config|
-  
-end
